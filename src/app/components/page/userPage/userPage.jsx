93424f9a3b9143b569bb17e5221c40298baca028
sergei2010 <sergejmatvejcuk323@gmail.com>
2021-10-29 23:19:46 +0200

    change userPageEdit.jsx for update new data

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -27,19 +27,23 @@ const UserPage = (userId) => {
             {user ? (
                 <div className="d-flex flex-column flex-shrink-0 p-3">
                     <h1>{user.name}</h1>
-                    <h2>Профессия: {user.profession.name}</h2>
+                    <h3>
+                        Электронная почта:{" "}
+                        {user.email ? user.email : "Нет данных"}
+                    </h3>
+                    <h3>Профессия: {user.profession.name}</h3>
+                    <h3>Sex: {user.sex ? user.sex : "Нет данных"}</h3>
                     <span>
                         <Qualities qualities={user.qualities} />
                     </span>
                     <h3>completedMeetings: {user.completedMeetings}</h3>
                     <h3>rate: {user.rate}</h3>
-                    <Link to={`/users/${user._id}/edit`}>
-                        <button
-                            className="btn btn-secondary mt-2"
-                            disabled={loading}
-                        >
-                            Редактировать
-                        </button>
+                    <Link
+                        to={`/users/${user._id}/edit`}
+                        className="btn btn-secondary mt-2"
+                        disabled={loading}
+                    >
+                        Редактировать
                     </Link>
                 </div>
             ) : (
